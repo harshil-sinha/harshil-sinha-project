18a9a2b1d5f0b9b514b5ec8779980143a5791cfd
harshil-sinha <130955042+harshil-sinha@users.noreply.github.com>
2025-01-10 19:42:15 +0530

    Update emailConfig.ts

--- config/emailConfig.ts
+++ config/emailConfig.ts
@@ -3,7 +3,7 @@ import nodemailer, { Transporter } from "nodemailer";
 const emailConfig: Transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: false, 
+  secure: false,
   auth: {
     user: "harshilsinha17@gmail.com",
     pass: "jtdhqxphaxoqdagn",
