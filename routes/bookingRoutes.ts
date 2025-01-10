6c082c0164d0255da499077577b24609adb95a59
harshil-sinha <130955042+harshil-sinha@users.noreply.github.com>
2025-01-10 19:48:32 +0530

    Update bookingRoutes.ts

--- routes/bookingRoutes.ts
+++ routes/bookingRoutes.ts
@@ -1,5 +1,9 @@
 import express, { Request, Response } from "express";
-import { createBooking, cancelTicket } from "../controllers/bookingController";
+import {
+  createBooking,
+  cancelTicket,
+  getTicketsByEmail,
+} from "../controllers/bookingController";
 
 const router = express.Router();
 
@@ -14,28 +18,35 @@ interface CancelTicketRequestBody {
   bookingId: string;
 }
 
-router.post("/create", async (req: Request<{}, {}, CreateBookingRequestBody>, res: Response) => {
-  try {
-    await createBooking(req, res);
-  } catch (error) {
-    console.error("Error during booking creation:", error);
-    res.status(500).json({
-      success: false,
-      message: "Internal server error during booking creation.",
-    });
+router.post(
+  "/create",
+  async (req: Request<{}, {}, CreateBookingRequestBody>, res: Response) => {
+    try {
+      await createBooking(req, res);
+    } catch (error) {
+      console.error("Error during booking creation:", error);
+      res.status(500).json({
+        success: false,
+        message: "Internal server error during booking creation.",
+      });
+    }
   }
-});
+);
 
-router.post("/cancel", async (req: Request<{}, {}, CancelTicketRequestBody>, res: Response) => {
-  try {
-    await cancelTicket(req, res);
-  } catch (error) {
-    console.error("Error during booking cancellation:", error);
-    res.status(500).json({
-      success: false,
-      message: "Internal server error during booking cancellation.",
-    });
+router.post(
+  "/cancel",
+  async (req: Request<{}, {}, CancelTicketRequestBody>, res: Response) => {
+    try {
+      await cancelTicket(req, res);
+    } catch (error) {
+      console.error("Error during booking cancellation:", error);
+      res.status(500).json({
+        success: false,
+        message: "Internal server error during booking cancellation.",
+      });
+    }
   }
-});
+);
+router.get("/tickets/:email", getTicketsByEmail);
 
 export default router;
