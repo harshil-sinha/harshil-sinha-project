f63ff52b751330a2f58ad2509fb9d1dbb462e231
harshil-sinha <130955042+harshil-sinha@users.noreply.github.com>
2025-01-10 19:12:58 +0530

    Update loginSchema.ts

--- Validations/loginSchema.ts
+++ Validations/loginSchema.ts
@@ -2,7 +2,10 @@ import * as Joi from "joi";
 
 const loginSchema = Joi.object({
   email: Joi.string().email().required(),
-  password: Joi.string().required(),
+  password: Joi.string()
+    .min(8)
+    .pattern(/[A-Za-z0-9]/)
+    .required(),
 });
 
 export default loginSchema;
